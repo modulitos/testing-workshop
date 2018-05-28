6abd4b6b4a0d2aa8b43ce4c7e21571f113ba8a00
modulitos <luke@lukeswart.net>
2018-05-27 18:33:06 -0700

    have babel transpile the imports in testing environment

--- other/configuration/calculator/.babelrc.js
+++ other/configuration/calculator/.babelrc.js
@@ -1,5 +1,14 @@
+const isTest = String(process.env.NODE_ENV) === 'test'
+
+// for the env plugin modules config:
+// isTest ? 'commonjs' : false
+
+// For dynamic import config in plugins
+// isTest ? 'dynamic-import-node' : null
 module.exports = {
-  presets: [['env', {modules: false}], 'react'],
+  // use modules:false here b/c webpack support esm and this allows webpack to do "tree shaking"
+  // presets: [['env', {modules: false}], 'react'],
+  presets: [['env', {modules: isTest ? 'commonjs' : false}], 'react'],
   plugins: [
     'syntax-dynamic-import',
     'transform-class-properties',
